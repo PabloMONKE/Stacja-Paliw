43844ec2e1f1689a0a66ba3b9c83098ec8c2f9b3
PabloMONKE <91789602+PabloMONKE@users.noreply.github.com>
2026-01-12 08:49:14 +0100

    Delete Paliwo.cs

--- Paliwo.cs
+++
@@ -1,7 +0,0 @@
-public class Paliwo
-{
-    public int IdPaliwa { get; set; }
-    public string Nazwa { get; set; }
-    public decimal CenaZaLitr { get; set; }
-    // W JSON nie potrzebujemy metod FromString/ToString
-}
