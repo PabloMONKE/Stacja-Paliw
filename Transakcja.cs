c2f09b97e761d55a06a3121d49298a761deceb2e
PabloMONKE <91789602+PabloMONKE@users.noreply.github.com>
2026-01-18 20:15:46 +0100

    Enhance Transakcja class with invoice support
    Added properties for invoice details and updated ToString method.

--- Transakcja.cs
+++ Transakcja.cs
@@ -7,13 +7,21 @@ namespace StacjaBenzynowa
         public Guid Id { get; set; }
         public DateTime Data { get; set; }
         public RodzajPaliwa Paliwo { get; set; }
-        
         public double Litry { get; set; }
         public decimal Cena { get; set; }
         public decimal Kwota { get; set; }
         public bool Oplacona { get; set; }
+        //Do faktury
+        public bool CzyFaktura { get; set; }
+        public string Nabywca { get; set; } //Null jeśli paragon
+        public string Adres { get; set; }
+        public string Nip { get; set; }
 
-        public override string ToString() => 
-            $"{Data} | {Paliwo} | {Litry:F2}L | {Kwota:C} | {(Oplacona ? "OK" : "BRAK WPŁATY")}";
+        public override string ToString()
+        {
+            string dok = CzyFaktura ? $"FAKTURA ({Nip})" : "PARAGON";
+            string status = Oplacona ? "OK" : "BRAK WPŁATY";
+            return $"{Data} | {Paliwo} | {Litry:F2}L | {Kwota:C} | {dok} | {status}";
+        }
     }
 }
